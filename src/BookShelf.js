532f91d78a06803d57b7c4783505b8bdf956bd76
magda-korzeniowska <m.korzeniowska@tlen.pl>
2018-11-14 18:03:03 +0100

    Adjust BookShelf component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,36 +4,14 @@ import Book from './Book'
 class BookShelf extends Component {
   render() {
     return(
-      <div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Currently Reading</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              <li>
-                <Book />
-              </li>
-            </ol>
-          </div>
-        </div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Want to Read</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              <li>
-                <Book />
-              </li>
-            </ol>
-          </div>
-        </div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Read</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              <li>
-                <Book />
-              </li>
-            </ol>
-          </div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.bookShelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+              <Book />
+            </li>
+          </ol>
         </div>
       </div>
     )
