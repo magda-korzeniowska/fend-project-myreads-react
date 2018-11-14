87c218855efcbec5bb11c2354ca88e0a942add21
magda-korzeniowska <m.korzeniowska@tlen.pl>
2018-11-14 16:44:36 +0100

    Fetch data from BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React from 'react'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  // state = {
+  //   /**
+  //    * TODO: Instead of using this state variable to keep track of which page
+  //    * we're on, use the URL in the browser's address bar. This will ensure that
+  //    * users can use the browser's back and forward buttons to navigate between
+  //    * pages, as well as provide a good URL they can bookmark and share.
+  //    */
+  //   showSearchPage: false
+  // }
+
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books:  []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (        
-          <SearchBooks />
-        ) : (
-          <ListBooks />
-        )}
+        <ListBooks
+          listOfBooks={this.state.books}
+        />
       </div>
     )
   }
