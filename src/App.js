3471b2c97b1cded742467f1c3a718ccede1570d1
magda-korzeniowska <m.korzeniowska@tlen.pl>
2018-11-16 16:21:55 +0100

    Add Link component

--- src/App.js
+++ src/App.js
@@ -36,10 +36,10 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {/* <ListBooks
+        <ListBooks
           books={this.state.books}
           changeShelf={this.changeShelf}
-        /> */}
+        />
         <SearchBooks
           books={this.state.books}
           changeShelf={this.changeShelf}
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Book from './Book'
 import * as BooksAPI from './BooksAPI'
 
@@ -32,7 +33,7 @@ class SearchBooks extends Component {
     return(
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link to='/' className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
