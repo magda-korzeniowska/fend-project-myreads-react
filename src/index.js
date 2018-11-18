08b0010f937de47eed869bd7a55eff2e671e56fb
magda-korzeniowska <m.korzeniowska@tlen.pl>
2018-11-18 18:05:25 +0100

    Add configuration for deploying app to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// I used HashRouter instead of BrowserRouter to make it possible to deploy
+// the app to gh-pages and have the app preview in README.md
+// I was trying to find a way of getting BrowserRouter to work on gh-pages,
+// but, for a time being, I haven't succeeded. 
+
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <HashRouter><App /></HashRouter>,
   document.getElementById('root')
 )
